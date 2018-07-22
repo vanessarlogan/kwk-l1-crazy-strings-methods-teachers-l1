45b1ccbb1880228870dd58c08efb7fea746f742a
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-07-22 18:25:50 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,11 @@
 # write your method here
 
 def crazy_strings(a,b)
-
+  anew = a.reverse
+  areallynew = anew.upcase
+  bnew = b.swapcase
+  breallhynew = bnew.tr("S", "Z")
+  puts areallynew + breallynew
 end
+
+crazy_strings("apple", "Friends")

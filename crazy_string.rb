2bee30f0f6f316cf2b474703e607032474dabb78
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-07-24 14:39:08 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,10 +2,16 @@
 
 def crazy_strings(a,b)
   anew = a.reverse
+ # puts anew
   areallynew = anew.upcase
+  #puts areallynew
   bnew = b.swapcase
-  breallhynew = bnew.tr("S", "Z")
+  #puts bnew
+  breallynew = bnew.tr("S", "Z")
+  #puts breallynew
   puts areallynew + breallynew
 end
 
 crazy_strings("apple", "Friends")
+
+#weird error messages
